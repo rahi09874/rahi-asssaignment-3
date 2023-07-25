12fd3a15fd2e7d81c4df057f09f73592d0b7a9a3
rahi09874 <rahiarafath612@gmail.com>
2023-07-25 03:28:25 +0100

    rahi

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="flex ">
+<div><img src="images/fruit2.png" alt="">
+  <p>orenge</p>
+</div>
+<div>
+  <img src="images/fruit3.png" alt="">
+  <p>Mango</p>
+</div>
+<div>
+  <img src="images/fruit4.png" alt="">
+  <p>strobery</p>
+</div>
+
+</div>
